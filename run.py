ac17d50f275a15e16eec34657a3725667a01c28c
blasf1 <blas.gomez@uclm.es>
2021-04-14 15:37:23 +0200

    Add all countries in a thread of tweets

--- run.py
+++ run.py
@@ -32,18 +32,13 @@ except:
 # get current percentage
 data = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv', parse_dates=['date'])
 
-print(data[data.location == 'European Union'])
-
 data_filtered = data[data.location == 'European Union']
 data_filtered = data_filtered[data_filtered.date == data_filtered.date.max()]
 
 # take last item in case dataset contains multiple items this day:
 percentage = data_filtered.iloc[-1].people_vaccinated_per_hundred
 
-# cycle world emojis each day:
-world_today = ":EU:"
-
-def tweet_bar_string_from_percentage(percentage, country, bar_format='|{bar:10}| {percentage:.1f}% COUNTRY'):
+def tweet_bar_string_from_percentage(percentage, country, bar_format='|{bar:10}| {percentage:04.1f}% COUNTRY'):
     bar = tqdm(initial=percentage, total=100., bar_format=bar_format, ascii=False)
     bar_string = str(bar)
     bar.close()
@@ -52,39 +47,61 @@ def tweet_bar_string_from_percentage(percentage, country, bar_format='|{bar:10}|
     tweet_string = tweet_string.replace('COUNTRY', flag.flagize(country))
     return tweet_string
 
-print(percentage)
-tweet_string = "People vaccinated with at least one dose\n" + tweet_bar_string_from_percentage(percentage, world_today) + '\n'
+tweet_string = "People vaccinated with at least one dose\n" + tweet_bar_string_from_percentage(percentage, ":EU:") + '\n'
 
 total_pop = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/scripts/input/un/population_2020.csv')
 
-supported_countries = {'Germany':":DE::EU:", 'France':":FR::EU:", 'Italy':":IT::EU:", 'Spain':":ES::EU:", 'Poland':":PL::EU:", 'Romania':":RO::EU:"}#, 'Netherlands':":NL:", 'Belgium':":BE:"}
+supported_countries = {'Germany':":DE::EU:", 'France':":FR::EU:", 'Italy':":IT::EU:", \
+                        'Spain':":ES::EU:", 'Poland':":PL::EU:", 'Romania':":RO::EU:", \
+                        'Netherlands':":NL::EU:", 'Belgium':":BE::EU:", 'Greece':":GR::EU:", \
+                        'Czechia':":CZ::EU:", 'Portugal':":PT::EU:", 'Sweden':":SE::EU:", \
+                        'Hungary':":HU::EU:",'Austria':":AT::EU:", 'Bulgaria':":BG::EU:", \
+                        'Denmark':":DK::EU:", 'Finland':":FI::EU:", 'Slovakia':":SK::EU:",\
+                        'Ireland':":IE::EU:", 'Croatia':":HR::EU:", 'Lithuania':":LT::EU:",\
+                        'Slovenia':":SI::EU:", 'Latvia':":LV::EU:", 'Estonia':":EE::EU:", \
+                        'Cyprus':":CY::EU:", 'Luxembourg':":LU::EU:", 'Malta':":MT::EU:"}
 countries_percentages = {}
 strings = {}
+previous_tweet = 0
 for country, emoji in supported_countries.items():
     data_filtered = data[data.location == country]
     data_filtered = data_filtered[data_filtered.date == data_filtered.date.max()]
     countries_percentages[country] = data_filtered.iloc[-1].people_vaccinated_per_hundred
     tweet_string_add = tweet_bar_string_from_percentage(countries_percentages[country], emoji)
     tweet_string = tweet_string + "\n" + tweet_string_add
+    if len(tweet_string) > 180:
+        if previous_tweet == 0:
+            previous_tweet = api.update_status(tweet_string)
+        else:
+            previous_tweet= api.update_status(status=tweet_string, in_reply_to_status_id=previous_tweet.id, auto_populate_reply_metadata=True)
+        print(tweet_string)
+        tweet_string = ""
 
+print(tweet_string)
+previous_tweet= api.update_status(status=tweet_string, in_reply_to_status_id=previous_tweet.id, auto_populate_reply_metadata=True)
+
+data_filtered = data[data.location == 'European Union']
+data_filtered = data_filtered[data_filtered.date == data_filtered.date.max()]
 percentage = data_filtered.iloc[-1].people_fully_vaccinated_per_hundred
-tweet_string_2 = "\nPeople fully vaccinated (2 doses)\n" + tweet_bar_string_from_percentage(percentage, world_today) + '\n'
+tweet_string_2 = "\nPeople fully vaccinated (2 doses)\n" + tweet_bar_string_from_percentage(percentage, ":EU:") + '\n'
 
 countries_percentages = {}
 strings = {}
+previous_tweet = 0
 for country, emoji in supported_countries.items():
     data_filtered = data[data.location == country]
     data_filtered = data_filtered[data_filtered.date == data_filtered.date.max()]
     countries_percentages[country] = data_filtered.iloc[-1].people_fully_vaccinated_per_hundred
     tweet_string_add = tweet_bar_string_from_percentage(countries_percentages[country], emoji)
     tweet_string_2 = tweet_string_2 + "\n" + tweet_string_add
+    if len(tweet_string_2) > 180:
+        if previous_tweet == 0:
+            previous_tweet = api.update_status(tweet_string_2)
+        else:
+            print("Reply to previous tweet")
+            previous_tweet= api.update_status(status=tweet_string_2, in_reply_to_status_id=previous_tweet.id, auto_populate_reply_metadata=True)
+        print(tweet_string_2)
+        tweet_string_2 = ""
 
-print("final string:")
-print(tweet_string)
-print("tweet length:", len(tweet_string))
 print(tweet_string_2)
-print("tweet length:", len(tweet_string_2))
-
-# and update on twitter
-original_tweet = api.update_status(tweet_string)
-reply1_tweet = api.update_status(status=tweet_string_2, in_reply_to_status_id=original_tweet.id, auto_populate_reply_metadata=True)
+previous_tweet= api.update_status(status=tweet_string_2, in_reply_to_status_id=previous_tweet.id, auto_populate_reply_metadata=True)
