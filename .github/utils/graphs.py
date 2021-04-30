d50afee69bd0c0444e51c07162fb483a901cd06f
blasf1 <blas.gomez@uclm.es>
2021-04-30 11:19:12 +0200

    [ADD] Generate first graph

--- .github/utils/graphs.py
+++ .github/utils/graphs.py
@@ -12,14 +12,13 @@ import sys
 # Third party
 import tweepy
 import pandas as pd
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import datetime
 
 # Local application
-from statistics import (read_data,
-                        get_population,
+from statistics import (get_population,
                         get_data_hundred_people)
 
 # =============================================================================
@@ -57,19 +56,33 @@ FLAGS = {
     "Spain": ":ES:",
     "Sweden": ":SE:"
 }
+
+COLUMNS = ["date", "total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "country"]
 # =============================================================================
 # Functions
 # =============================================================================
 
 def read_data(path):
-    """Read the vaccination data for a country."""
-    # Use the date to identify the vaccination data
-    path = os.path.join(path, country + ".csv")
-
+    """Read the last vaccination data for all countries."""
     index_col = "date"
-    data = pd.read_csv(path, index_col=index_col)
+    data = pd.DataFrame(columns = COLUMNS)
+    for country in FLAGS.keys():
+        path_file = os.path.join(path, country.replace(" ", "") + ".csv")
+        data_country = pd.read_csv(path_file)
+        data_country = data_country.iloc[-1]#.insert(1, "country", [country] * len(data_country), False)
+        data_country["country"] = country
+        data = data.append(data_country)
 
     return data
+
+def plot_data(data, parameter):
+    """Plot data in parameter for all countries in dataframe"""
+
+    x = "country"
+    kind = "bar"
+    
+    data.plot(x = x, y = parameter, kind = kind)
+    plt.show()
 
 # =============================================================================
 # Arguments
@@ -78,46 +91,46 @@ def read_data(path):
 description = "Publish vaccination data for a country."
 parser = argparse.ArgumentParser(description=description)
 
-arg = "--country"
-parser.add_argument(arg)
-
 arg = "--data"
 parser.add_argument(arg)
 
-arg = "--output"
-parser.add_argument(arg)
+# arg = "--output"
+# parser.add_argument(arg)
 
-arg = "--population"
-parser.add_argument(arg)
+# arg = "--population"
+# parser.add_argument(arg)
 
-arg = "--api"
-default = os.environ.get("BOT_API")
-parser.add_argument(arg, default=default)
+# arg = "--api"
+# default = os.environ.get("BOT_API")
+# parser.add_argument(arg, default=default)
 
-arg = "--api-secret"
-default = os.environ.get("BOT_API_SECRET")
-parser.add_argument(arg, default=default)
+# arg = "--api-secret"
+# default = os.environ.get("BOT_API_SECRET")
+# parser.add_argument(arg, default=default)
 
-arg = "--access"
-default = os.environ.get("BOT_ACCESS")
-parser.add_argument(arg, default=default)
+# arg = "--access"
+# default = os.environ.get("BOT_ACCESS")
+# parser.add_argument(arg, default=default)
 
-arg = "--access-secret"
-default = os.environ.get("BOT_ACCESS_SECRET")
-parser.add_argument(arg, default=default)
+# arg = "--access-secret"
+# default = os.environ.get("BOT_ACCESS_SECRET")
+# parser.add_argument(arg, default=default)
 
 args = sys.argv[1:]
 args = parser.parse_args(args)
 
 # Rename the command line arguments for easier reference
-country = args.country
+#country = args.country
 data = args.data
-output = args.output
-population = args.population
-api = args.api
-api_secret = args.api_secret
-access = args.access
-access_secret = args.access_secret
+#output = args.output
+#population = args.population
+#api = args.api
+#api_secret = args.api_secret
+#access = args.access
+#access_secret = args.access_secret
 
 #Read data
 data = read_data(data)
+print(data)
+#Plot
+plot_data(data, "total_vaccinations")
