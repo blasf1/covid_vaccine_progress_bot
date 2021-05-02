79b86507834dabca705c114de422eda10619ca8f
blasf1 <blas.gomez@uclm.es>
2021-05-02 13:26:44 +0200

    Reorganize code

--- .github/utils/graphs.py
+++ .github/utils/graphs.py
@@ -14,7 +14,7 @@ import glob
 import tweepy
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.offsetbox import OffsetImage,AnnotationBbox
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import matplotlib
 import numpy as np
 import datetime
@@ -60,6 +60,7 @@ FLAGS = {
 # Functions
 # =============================================================================
 
+
 def get_population(path, country):
     """Get the population for a country."""
     # Use the country to identify the population data
@@ -83,7 +84,8 @@ def get_data_hundred_people(data, path):
 def read_data(path, path_population):
     """Read the last vaccination data for all countries."""
     files = glob.glob(path + "*.csv")
-    read_csv = lambda file: get_data_hundred_people(pd.read_csv(file).iloc[[-1]], path_population)
+    def read_csv(file): return get_data_hundred_people(
+        pd.read_csv(file).iloc[[-1]], path_population)
 
     data = pd.concat(map(read_csv, files))
 
@@ -120,10 +122,11 @@ def plot_data(data, unit, parameter, title, output, flags):
     plt.rcParams['font.sans-serif'] = "Arial"
     plt.rcParams['font.family'] = "sans-serif"
 
-    data_to_plot = data[["location", parameter]].sort_values(by=parameter, ascending=True).dropna()
+    data_to_plot = data[["location", parameter]].sort_values(
+        by=parameter, ascending=True).dropna()
 
     ax = data_to_plot.plot.barh(x=x, y=parameter, figsize=figsize,
-                                legend=legend, width=width, xlabel="", 
+                                legend=legend, width=width, xlabel="",
                                 fontsize=16, color="#3C4E66")
 
     # Despine
@@ -135,31 +138,36 @@ def plot_data(data, unit, parameter, title, output, flags):
     ax.xaxis.set_label("")
 
     # Title
-    ax.set_title(title + ", " + datetime.datetime.now().strftime("%d-%m-%Y"), fontsize = 28, loc = "left", fontname = "Arial", fontweight = "bold", pad = 10)
+    ax.set_title(title + ", " + datetime.datetime.now().strftime("%d-%m-%Y"),
+                 fontsize=28, loc="left", fontname="Arial", fontweight="bold", pad=10)
 
     # Draw vertical axis lines
     vals = ax.get_xticks()
     for tick in vals:
-        ax.axvline(x=tick, linestyle='dashed', alpha=0.2, color='#293133', zorder=0)
+        ax.axvline(x=tick, linestyle='dashed',
+                   alpha=0.2, color='#293133', zorder=0)
 
     # Show number at the end of the bar
-    [ax.text(v + 0.4, i - (width / 4), "{:.2f}".format(v) + unit, fontsize=16) for i, v in enumerate(data[parameter])]
+    [ax.text(v + 0.4, i - (width / 4), "{:.2f}".format(v) +
+             unit, fontsize=16) for i, v in enumerate(data[parameter])]
 
     # configure y axis labels (add flags)
-    ax.tick_params(axis = "y", which = "both", left = False, right = False, pad = 10, size = 20)
-    ax.tick_params(axis = "x", which = "both", bottom = False, top = False)
+    ax.tick_params(axis="y", which="both", left=False,
+                   right=False, pad=10, size=20)
+    ax.tick_params(axis="x", which="both", bottom=False, top=False)
     for i, c in enumerate(data["location"]):
         offset_image(i, c, ax, flags)
 
     file = os.path.join(output + title.replace(" ", "_") + ".png")
-    plt.figtext(0.01,0.01,"@VaccinationEu\nSource: Our World in Data")
+    plt.figtext(0.01, 0.01, "@VaccinationEu\nSource: Our World in Data")
     plt.tight_layout()
-    
+
     plt.savefig(file, dpi=300)
 
 # =============================================================================
 # Arguments
 # =============================================================================
+
 
 description = "Publish vaccination data for a country."
 parser = argparse.ArgumentParser(description=description)
@@ -225,10 +233,10 @@ api = tweepy.API(auth)
 include_email = False
 user = api.verify_credentials(include_email=include_email)
 
-#Read data
+# Read data
 data = read_data(data, population)
 
-#Plot
+# Plot
 title1 = "Doses administered per 100 people"
 plot_data(data, "", "total_vaccinations", title1, output, flags)
 
@@ -238,9 +246,9 @@ plot_data(data, "", "total_vaccinations", title1, output, flags)
 # title3 = "% population vaccinated with at least one dose"
 # plot_data(data, "%", "people_vaccinated", title3, output)
 
-tweet = (emoji.emojize(":calendar::bar_chart:") 
-        + " It's time for a daily sum up! "
-        +emoji.emojize(":syringe:"))
+tweet = (emoji.emojize(":calendar::bar_chart:")
+         + " It's time for a daily sum up! "
+         + emoji.emojize(":syringe:"))
 
 print(tweet)
 
