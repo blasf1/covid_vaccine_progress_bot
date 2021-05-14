60dcd6d76b230cdba273108614a0aaf75034be8e
Blas Gomez <blas.gomez@uclm.es>
2021-05-14 17:26:42 +0200

    [FMT] Comments

--- .github/utils/update.py
+++ .github/utils/update.py
@@ -1,3 +1,6 @@
+# =============================================================================
+# Imports
+# =============================================================================
 import argparse
 import os
 import sys
@@ -7,79 +10,82 @@ from vax.cmd import main_get_data
 from vax.utils.paths import Paths
 from vax.cmd.get_data import country_to_module
 
+# =============================================================================
+# Constants
+# =============================================================================
+SKIPPED_COUNTRIES = ['australia',
+                     'canada',
+                     'chile',
+                     'ecuador',
+                     'hong_kong',
+                     'israel',
+                     'jersey',
+                     'new_zealand',
+                     'peru',
+                     'saudi_arabia',
+                     'spc',
+                     'switzerland',
+                     'trinidad_and_tobago',
+                     'united_kingdom',
+                     'uruguay',
+                     'argentina',
+                     'aruba',
+                     'azerbaijan',
+                     'bahrain',
+                     'bolivia',
+                     'brazil',
+                     'cayman_islands',
+                     'colombia',
+                     'costa_rica',
+                     'curacao',
+                     'dominican_republic',
+                     'el_salvador',
+                     'equatorial_guinea',
+                     'faeroe_islands',
+                     'ghana',
+                     'greenland',
+                     'guatemala',
+                     'iceland',
+                     'india',
+                     'isle_of_man',
+                     'japan',
+                     'kazakhstan',
+                     'lebanon',
+                     'macao',
+                     'malaysia',
+                     'moldova',
+                     'mongolia',
+                     'montenegro',
+                     'morocco',
+                     'nepal',
+                     'north_macedonia',
+                     'northern_cyprus',
+                     'pakistan',
+                     'philippines',
+                     'qatar',
+                     'russia',
+                     'saint_lucia',
+                     'san_marino',
+                     'serbia',
+                     'singapore',
+                     'south_africa',
+                     'south_korea',
+                     'sri_lanka',
+                     'suriname',
+                     'taiwan',
+                     'thailand',
+                     'turkey',
+                     'ukraine',
+                     'united_arab_emirates',
+                     'united_states',
+                     'who',
+                     'austria']
 
-SKIPPED_COUNTRIES =['australia', 
-            'canada', 
-            'chile', 
-            'ecuador', 
-            'hong_kong', 
-            'israel', 
-            'jersey', 
-            'new_zealand', 
-            'peru', 
-            'saudi_arabia', 
-            'spc', 
-            'switzerland', 
-            'trinidad_and_tobago', 
-            'united_kingdom', 
-            'uruguay', 
-            'argentina', 
-            'aruba', 
-            'azerbaijan', 
-            'bahrain', 
-            'bolivia', 
-            'brazil', 
-            'cayman_islands', 
-            'colombia', 
-            'costa_rica', 
-            'curacao', 
-            'dominican_republic', 
-            'el_salvador', 
-            'equatorial_guinea', 
-            'faeroe_islands', 
-            'ghana', 
-            'greenland', 
-            'guatemala', 
-            'iceland', 
-            'india', 
-            'isle_of_man', 
-            'japan', 
-            'kazakhstan', 
-            'lebanon', 
-            'macao', 
-            'malaysia', 
-            'moldova', 
-            'mongolia', 
-            'montenegro', 
-            'morocco', 
-            'nepal', 
-            'north_macedonia', 
-            'northern_cyprus', 
-            'pakistan', 
-            'philippines', 
-            'qatar', 
-            'russia', 
-            'saint_lucia', 
-            'san_marino', 
-            'serbia', 
-            'singapore', 
-            'south_africa', 
-            'south_korea', 
-            'sri_lanka', 
-            'suriname', 
-            'taiwan', 
-            'thailand', 
-            'turkey', 
-            'ukraine', 
-            'united_arab_emirates', 
-            'united_states', 
-            'who']
 
 
 # =============================================================================
-# Main
+# Arguments
 # =============================================================================
-
 description = "Update data for all countries."
 parser = argparse.ArgumentParser(description=description)
 
@@ -92,6 +98,9 @@ args = parser.parse_args(args)
 
 project_dir = args.directory
 
+# =============================================================================
+# Main
+# =============================================================================
 paths = Paths(project_dir)
 
-main_get_data(paths, n_jobs = 4, skip_countries=SKIPPED_COUNTRIES)
+main_get_data(paths, n_jobs=4, skip_countries=SKIPPED_COUNTRIES)
