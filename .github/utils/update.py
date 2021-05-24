16f7be3aadea57273703fbc2449bbb5fe9221fd9
Blas Gomez <blas.gomez@uclm.es>
2021-05-24 21:03:36 +0200

    [FIX] Missing comma

--- .github/utils/update.py
+++ .github/utils/update.py
@@ -14,7 +14,7 @@ from vax.cmd.get_data import country_to_module
 # Constants
 # =============================================================================
 SKIPPED_COUNTRIES = ['australia',
-                     'bangladesh'
+                     'bangladesh',
                      'canada',
                      'chile',
                      'ecuador',
