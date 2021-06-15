6afc53dc37989794fdecd8e7a5fd552445184788
Blas Gomez <blas.gomez@uclm.es>
2021-06-15 11:28:26 +0200

    Revert "[FIX] correct old data before updating"
    This reverts commit 84dccb5d9ae391e2e485bf4bf17c1e37ee798fba.

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -2,34 +2,6 @@
 
 set -e
 
-COUNTRIES="Austria
-Belgium
-Bulgaria
-Croatia
-Cyprus
-Czechia
-Denmark
-Estonia
-Finland
-France
-Germany
-Greece
-Hungary
-Ireland
-Italy
-Latvia
-Lithuania
-Luxembourg
-Malta
-Netherlands
-Poland
-Portugal
-Romania
-Slovakia
-Slovenia
-Spain
-Sweden"
-
 # SOURCE=$(python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/source.py $COUNTRY)
 
 # Move to the directory with the vaccination scripts to install the package
@@ -41,9 +13,5 @@ cd
 
 cp -r $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/src/vax/_static /opt/hostedtoolcache/Python/3.9.5/x64/lib/python3.9/site-packages/vax/
 
-for f in $COUNTRIES
-do
-    cp $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/output/$f.csv $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/
-done
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/update.py
