cfa9a8749b6d0e5c94c964144793f56b0a134669
blasf1 <blas.gomez@uclm.es>
2021-07-06 12:57:49 +0200

    [FIX]

--- .github/utils/run.bash
+++ .github/utils/run.bash
@@ -2,7 +2,34 @@
 
 set -e
 
-COUNTRIES=(Austria Belgium Bulgaria Croatia Cyprus Czechia Denmark Estonia Finland France Germany Greece Hungary Italy Latvia Lithuania Luxembourg Malta Netherlands Poland Portugal Romania Slovakia Slovenia Spain Sweden "United Kingdom" "United States")
+declare -a COUNTRIES=("Austria"\
+"Belgium"\
+"Bulgaria"\
+"Croatia"\
+"Cyprus"\
+"Czechia"\
+"Denmark"\
+"Estonia"\
+"Finland"\
+"France"\
+"Germany"\
+"Greece"\
+"Hungary"\
+"Italy"\
+"Latvia"\
+"Lithuania"\
+"Luxembourg"\
+"Malta"\
+"Netherlands"\
+"Poland"\
+"Portugal"\
+"Romania"\
+"Slovakia"\
+"Slovenia"\
+"Spain"\
+"Sweden"\
+"United Kingdom"\
+"United States")
 #Ireland
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/publish.py --data $SCRIPTS/output \
@@ -10,7 +37,7 @@ python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/publish.py --d
                                                                              --input $INPUT \
                                                                              --output $OUTPUT \
                                                                              --population $POPULATION
-for f in "${components[@]}"
+for f in $COUNTRIES
 do
     cp $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/output/$f.csv $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/
 done
