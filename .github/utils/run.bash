500ea0ccee101fc45c63d50996f2f41f961da9e0
blasf1 <blas.gomez@uclm.es>
2021-07-06 12:47:56 +0200

    [FIX] array

--- .github/utils/run.bash
+++ .github/utils/run.bash
@@ -2,34 +2,7 @@
 
 set -e
 
-COUNTRIES=(Austria
-Belgium
-Bulgaria
-Croatia
-Cyprus
-Czechia
-Denmark
-Estonia
-Finland
-France
-Germany
-Greece
-Hungary
-Italy
-Latvia
-Lithuania
-Luxembourg
-Malta
-Netherlands
-Poland
-Portugal
-Romania
-Slovakia
-Slovenia
-Spain
-Sweden
-"United Kingdom"
-"United States")
+COUNTRIES=(Austria Belgium Bulgaria Croatia Cyprus Czechia Denmark Estonia Finland France Germany Greece Hungary Italy Latvia Lithuania Luxembourg Malta Netherlands Poland Portugal Romania Slovakia Slovenia Spain Sweden "United Kingdom" "United States")
 #Ireland
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/publish.py --data $SCRIPTS/output \
