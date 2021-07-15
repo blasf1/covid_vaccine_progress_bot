51346a4bc052aeb104478d036de99a0509b36828
blasf1 <blas.gomez@uclm.es>
2021-07-16 01:03:23 +0200

    [FIX] Croatia

--- patch/incremental/croatia.py
+++ patch/incremental/croatia.py
@@ -16,6 +16,9 @@ def read(source: str) -> pd.Series:
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_argument("--headless")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--no-sandbox")
+
     driver = uc.Chrome(options = options)
     driver.implicitly_wait(15)
     driver.get(source)
