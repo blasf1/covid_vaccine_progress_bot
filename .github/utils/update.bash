06344965a9df5d43827077e4a4f3adc763765b84
blasf1 <blas.gomez@uclm.es>
2021-07-15 13:04:48 +0200

    [FIX] Forgot to remove -r

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -12,7 +12,7 @@ pip install .
 cd
 
 ls /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/
-cp -r $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/src/vax/_static/queries/ireland-* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/_static/queries/ireland-* ;
-cp -r $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/src/vax/_static/queries/poland-all.json /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/_static/queries/poland-all.json ;
+cp $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/src/vax/_static/queries/ireland-* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/_static/queries/ireland-* ;
+cp $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/src/vax/_static/queries/poland-all.json /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/_static/queries/poland-all.json ;
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/update.py
