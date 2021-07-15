cbe75bf94cb644aa926e03be4fd730e4b8c59df6
blasf1 <blas.gomez@uclm.es>
2021-07-15 13:16:15 +0200

    [FIX]

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -11,9 +11,8 @@ pip install .
 
 cd
 
-mkdir /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/_static
-mkdir /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/_static/queries/
-ls /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/
+
+ls /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/
 cp $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/src/vax/_static/queries/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/_static/queries/ ;
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/update.py
