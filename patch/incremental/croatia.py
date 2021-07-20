28a1939e9e71a6410cd34d4ac9cb8e3b0e435f11
blasf1 <blas.gomez@uclm.es>
2021-07-20 23:52:22 +0200

    [FIX] New try for Croatia

--- patch/incremental/croatia.py
+++ patch/incremental/croatia.py
@@ -18,6 +18,9 @@ def read(source: str) -> pd.Series:
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--enable-webgl")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = uc.Chrome(options = options)
     driver.get(source)
     content = driver.page_source
