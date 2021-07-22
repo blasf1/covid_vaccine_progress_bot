b6a20ff30b87b9fd87a4112d40a5be7c52d2d1e4
blasf1 <blas.gomez@uclm.es>
2021-07-22 18:11:52 +0200

    temporarily remove latvia

--- .github/utils/update.py
+++ .github/utils/update.py
@@ -13,7 +13,7 @@ from vax.cmd.get_data import country_to_module
 # =============================================================================
 # Constants
 # =============================================================================
-SKIPPED_COUNTRIES = ['hungary',
+SKIPPED_COUNTRIES = ['hungary','latvia',
                      'croatia',
                      'albania',
                      'africacdc',
