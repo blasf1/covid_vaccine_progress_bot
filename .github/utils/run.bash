0f740145c59c8fb3b2c31097a00da96113c1b3ab
Blas Gomez <blas.gomez@uclm.es>
2021-08-09 20:15:08 +0200

    FIX copy routes

--- .github/utils/run.bash
+++ .github/utils/run.bash
@@ -38,11 +38,11 @@ python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/publish.py --d
                                                                              --population $POPULATION
 for f in "${COUNTRIES[@]}";
 do
-    cp $GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/output/$f.csv $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/
+    cp $GITHUB_WORKSPACE/covid-19-data/scripts/output/vaccinations/main_data/$f.csv $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/
 done
 
-cp "$GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/output/United Kingdom.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/UnitedKingdom.csv
-cp "$GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/output/United States.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/UnitedStates.csv
-cp "$GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/output/Norway.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/Norway.csv
-cp "$GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/output/Iceland.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/Iceland.csv
-cp "$GITHUB_WORKSPACE/covid-19-data/scripts/scripts/vaccinations/output/Switzerland.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/Switzerland.csv
+cp "$GITHUB_WORKSPACE/covid-19-data/scripts/output/vaccinations/main_data/United Kingdom.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/UnitedKingdom.csv
+cp "$GITHUB_WORKSPACE/covid-19-data/scripts/output/vaccinations/main_data/United States.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/UnitedStates.csv
+cp "$GITHUB_WORKSPACE/covid-19-data/scripts/output/vaccinations/main_data/Norway.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/Norway.csv
+cp "$GITHUB_WORKSPACE/covid-19-data/scripts/output/vaccinations/main_data/Iceland.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/Iceland.csv
+cp "$GITHUB_WORKSPACE/covid-19-data/scripts/output/vaccinations/main_data/Switzerland.csv" $GITHUB_WORKSPACE/covid_vaccine_progress_bot/output/external/Switzerland.csv
