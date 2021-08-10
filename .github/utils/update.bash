eddd7891fd2bf4d98e4c3b2842ea414632365e16
Blas Gomez <blas.gomez@uclm.es>
2021-08-10 14:14:48 +0200

    FIX poland and ireland

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -10,7 +10,7 @@ cd $SCRIPTS
 pip install .
 
 ls /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/
-#cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/_static/queries/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/vax/_static/queries/ ;
+cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/_static/queries/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/_static/queries/ ;
 
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/update.py
