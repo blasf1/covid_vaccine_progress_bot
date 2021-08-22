995fb2f13803e499d73d5ff9d65de68e9141b88c
Blas Gomez <blas.gomez@uclm.es>
2021-08-22 20:08:25 +0200

    Sweden

--- patch/batch/sweden.py
+++ patch/batch/sweden.py
@@ -2,7 +2,7 @@ import datetime
 
 import pandas as pd
 
-from cowidev.vax.utils.utils import make_monotonic
+from cowidev.vax.utils.utils import make_monotonic, increment
 
 
 class Sweden(object):
@@ -20,10 +20,7 @@ class Sweden(object):
 
     def read(self) -> pd.DataFrame:
         daily = self._read_daily_data()
-        print(daily)
-        weekly = self._read_weekly_data()
-        weekly = weekly[weekly["date"] < daily["date"].min()]
-        return daily #pd.concat([daily, weekly]).sort_values("date").reset_index(drop=True)
+        return daily 
 
     def enrich_columns(self, df: pd.DataFrame) -> pd.DataFrame:
         return df.assign(location=self.location, source_url=self.source_url_daily)
@@ -36,40 +33,6 @@ class Sweden(object):
             df.pipe(self.enrich_vaccine).pipe(self.enrich_columns).pipe(make_monotonic)
         )
 
-    def _week_to_date(self, row: int):
-        origin_date = (
-            pd.to_datetime("2019-12-29")
-            if row.Vecka >= 52
-            else pd.to_datetime("2021-01-03")
-        )
-        return origin_date + pd.DateOffset(days=7 * int(row.Vecka))
-
-    def _read_weekly_data(self) -> pd.DataFrame:
-        df = pd.read_excel(self.source_url_weekly, sheet_name="Vaccinerade tidsserie")
-        df = df[df["Region"] == "| Sverige |"][
-            ["Vecka", "Antal vaccinerade", "Vaccinationsstatus"]
-        ]
-        df = df.pivot_table(
-            values="Antal vaccinerade", index="Vecka", columns="Vaccinationsstatus"
-        ).reset_index()
-        # Week-to-date logic will stop working after 2021
-        if not datetime.date.today().year < 2022:
-            raise ValueError("Check the year! This script is not ready for 2022!")
-        df.loc[:, "date"] = df.apply(self._week_to_date, axis=1).dt.date.astype(str)
-        df = (
-            df.drop(columns=["Vecka"])
-            .sort_values("date")
-            .rename(
-                columns={
-                    "Minst 1 dos": "people_vaccinated",
-                    "Färdigvaccinerade": "people_fully_vaccinated",
-                }
-            )
-        )
-        df.loc[:, "total_vaccinations"] = (
-            df["people_vaccinated"] + df["people_fully_vaccinated"]
-        )
-        return df
 
     def _read_daily_data(self) -> pd.DataFrame:
         df = pd.read_html(self.source_url_daily)[1]
@@ -120,8 +83,6 @@ class Sweden(object):
         df2["total_vaccinations"] = (
             df2["people_vaccinated"] + df2["people_fully_vaccinated"]
         )
-        print("DF2")
-        print(df2)
         df["people_vaccinated"] = (
             df["people_vaccinated"].str.replace(r"\s", "", regex=True).astype(int)
         )
@@ -133,16 +94,30 @@ class Sweden(object):
         )
         df = df.drop(df.loc[1:].index)
         df = df.set_index("date")
-        print(df)
         df = df + df2
-        print("DF")
-        print(df)
         return df
 
     def to_csv(self, paths):
         """Generalized."""
         df = self.read().pipe(self.pipeline)
         print(df)
+        output_file = paths.tmp_vax_out("Luxembourg")
+        previous_data = pd.read_csv(output_file)
+        if previous_data["total_vaccinations"].iloc[-1] >= df["total_vaccinations"]:
+            print("Luxembourg is up to date")
+            return
+
+        increment(
+            paths=paths,
+            location=df["location"],
+            total_vaccinations=df["total_vaccinations"],
+            people_vaccinated=df["people_vaccinated"],
+            people_fully_vaccinated=df["people_fully_vaccinated"],
+            date=df["date"],
+            source_url=df["source_url"],
+            vaccine=df["vaccine"],
+        )
+
         #df.to_csv(paths.tmp_vax_out(self.location), index=False)
 
 
