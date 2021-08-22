0af09a404af829f9bc1596e305c0648f866a5dbc
Blas Gomez <blas.gomez@uclm.es>
2021-08-22 16:27:12 +0200

    status is index

--- patch/batch/sweden.py
+++ patch/batch/sweden.py
@@ -105,7 +105,7 @@ class Sweden(object):
         aggregation_functions = {"date": "first", "people_vaccinated": "first"}
         df2 = df2.groupby(df2["status"]).aggregate(aggregation_functions)
         print(df2)
-        df2["people_fully_vaccinated"] = df2.loc[df2["status"] == "2 doser"]["people_vaccinated"]
+        df2["people_fully_vaccinated"] = df2.loc[df2.index == "2 doser"]["people_vaccinated"]
         print(df2)
         df["people_vaccinated"] = (
             df["people_vaccinated"].str.replace(r"\s", "", regex=True).astype(int) + df2["people_vaccinated"].str.replace(r"\s", "", regex=True).astype(int)
