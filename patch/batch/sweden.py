8ebd48a4ba4fa82cf1a8e6f0279823aa9233b0b0
Blas Gomez <blas.gomez@uclm.es>
2021-08-22 20:27:30 +0200

    fix sweden

--- patch/batch/sweden.py
+++ patch/batch/sweden.py
@@ -104,7 +104,7 @@ class Sweden(object):
         print(df)
         output_file = paths.tmp_vax_out("Sweden")
         previous_data = pd.read_csv(output_file)
-        if previous_data["total_vaccinations"].iloc[-1] >= df["total_vaccinations"]:
+        if previous_data["total_vaccinations"].iloc[-1] >= df["total_vaccinations"].iloc[-1]:
             print("Sweden is up to date")
             return
 
