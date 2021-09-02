c268281d5fdf10282da3481a4c2452e5403eb6ab
blasf1 <blas.gomez@uclm.es>
2021-09-02 17:16:22 +0200

    OWID, please, if ain't broken, don't change it

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -11,6 +11,6 @@ pip install .
 
 ls /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/
 cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/_static/queries/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/_static/queries/ ;
-
+cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/utils/orgs/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/utils/orgs/ ;
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/update.py
