a9950e3cfa44113e3681e6cfba0b5f0f4bd23b99
blasf1 <blas.gomez@uclm.es>
2021-09-02 17:21:12 +0200

    missing "

--- .github/utils/tweet.py
+++ .github/utils/tweet.py
@@ -82,7 +82,8 @@ def get_progress_bar(percentage, increment):
     """Get a progress bar string given a percentage."""
     initial = percentage
     total = 100
-    bar_format = generateProgressbar(percentage) + "\n" + {percentage: 04.1f} % " + f"[{increment: +03.1f}]"
+    bar_format = generateProgressbar(
+        percentage) + "\n" + "{percentage: 04.1f} % " + f"[{increment: +03.1f}]"
 
     with tqdm(initial=initial, total=total, bar_format=bar_format) as bar:
         # Convert the bar to string for concatenating
