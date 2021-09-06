f6f48cf327d2b544907e0fe018ee87cbd71c580c
Blas Gomez <blas.gomez@uclm.es>
2021-09-06 21:32:06 +0200

    Fix hungarian message

--- .github/utils/graphs.py
+++ .github/utils/graphs.py
@@ -423,7 +423,7 @@ reminder = (emoji.emojize(":pushpin:")
             + "Shares over the total population (not just adults)"
             + "\n\n"
             + emoji.emojize(":pushpin:")
-            + "Hungary is not providing data"
+            + "Hungary is not providing enough data"
             + "\n\n"
             + emoji.emojize(":pushpin:")
             + "Remember that this is just information, not a competition. We all are in this together"
