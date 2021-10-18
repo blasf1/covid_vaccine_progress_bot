6a93062eaf8df1ada5023b595d6a607d9daac7f7
Blas Gómez <blas.gomez@uclm.es>
2021-10-19 00:15:56 +0200

    Update update.py

--- .github/utils/update.py
+++ .github/utils/update.py
@@ -13,7 +13,7 @@ from cowidev.vax.cmd.get_data import country_to_module
 # =============================================================================
 # Constants
 # =============================================================================
-SKIPPED_COUNTRIES = ['croatia', 'hungary', 'latvia',
+SKIPPED_COUNTRIES = ['croatia', 'hungary', 'latvia', 'ireland',
                      'mexico',
                      'albania',
                      'africacdc',
