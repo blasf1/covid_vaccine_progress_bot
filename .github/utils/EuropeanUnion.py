7fde0415e9f1d5261b887f58d77c45e0bbdfd699
Blas Gomez <blas.gomez@uclm.es>
2021-12-02 21:02:09 +0100

    fix EU

--- .github/utils/EuropeanUnion.py
+++ .github/utils/EuropeanUnion.py
@@ -81,6 +81,8 @@ for country in COUNTRIES:
         print(country)
         print(data)
         data = data[~data.index.duplicated(keep='last')]
+        if 'total_boosters' not in data.columns:
+            data["total_boosters"] = 0
         data = data.reindex_like(eu_data).fillna(method="ffill")
         eu_data = eu_data.add(data, fill_value=0)
 
