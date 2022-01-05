f10f8b0f147b60884a212fcf036160664bd5720d
Blas Gomez <blas.gomez@uclm.es>
2022-01-05 10:33:54 +0100

    Fix cp

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -11,6 +11,6 @@ pip install .
 
 cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/_static/queries/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/_static/queries/ ;
 cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/utils/orgs/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/utils/orgs/ ;
-cp $GITHUB_WORKSPACE/covid_vaccine_progress_bot/get_data.py $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/cmd/get_data.py ;
-
+cp $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/get_data.py /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/cmd/get_data.py ;
+cat $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/cmd/get_data.py ;
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/update.py
