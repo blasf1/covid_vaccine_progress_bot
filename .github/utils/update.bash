c315b80fc623b602e9547ce5d2c8678a07f8a055
Blas Gomez <blas.gomez@uclm.es>
2022-01-05 10:26:10 +0100

    Fix owid constant changes

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -9,8 +9,8 @@ cd $SCRIPTS
 
 pip install .
 
-ls /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/
 cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/_static/queries/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/_static/queries/ ;
 cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/utils/orgs/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/utils/orgs/ ;
+cp $GITHUB_WORKSPACE/covid_vaccine_progress_bot/get_data.py $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/cmd/get_data.py ;
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/update.py
