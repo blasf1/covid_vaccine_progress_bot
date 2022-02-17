2475bd4589ea7c1f84ab45b4f25d6b4dab9d0e1a
Blas Gomez <blas.gomez@uclm.es>
2022-02-17 23:55:00 +0100

    Fix path

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -12,7 +12,7 @@ pip install .
 cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/_static/queries/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/_static/queries/ ;
 cp $GITHUB_WORKSPACE/covid-19-data/scripts/src/cowidev/vax/utils/orgs/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/utils/orgs/ ;
 cp -r $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/cowidev_old/* /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/cmd/ ;
-rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/incremental/colombia.py
+rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/incremental/incremental/colombia.py
 rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/batch/batch/ecdc.py
 rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/batch/batch/luxembourg.py
 rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/batch/batch/romania.py
