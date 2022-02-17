de78bf9fad13e9b331cffa387b2a6df73341a4b6
Blas Gomez <blas.gomez@uclm.es>
2022-02-17 23:43:47 +0100

    fix spc

--- .github/utils/update.bash
+++ .github/utils/update.bash
@@ -16,5 +16,6 @@ rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax
 rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/batch/batch/ecdc.py
 rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/batch/batch/luxembourg.py
 rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/batch/batch/romania.py
+rm /opt/hostedtoolcache/Python/3.9.6/x64/lib/python3.9/site-packages/cowidev/vax/batch/batch/spc.py
 
 python $GITHUB_WORKSPACE/covid_vaccine_progress_bot/.github/utils/update.py
